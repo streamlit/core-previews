8f627726cb1144ce9d17f30c81420e6a0d3bfd07
Austin Chen <akrolsmir@gmail.com>
2020-10-16 03:45:47 -0700

    Add links to the PR and wheel file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 from execbox import execbox
 
+with open('requirements.txt') as requirements:
+    s3_url = requirements.read().split('\n')[2]
+import re
+match = re.search('/pr-(\\d+)/', s3_url)
+if match: 
+    pr_number = match.group(1)
+    
+    col1, col2 = st.beta_columns(2)
+    col1.write(f"🔭 [View PR on Github](https://github.com/streamlit/streamlit/pull/{pr_number})")
+    col2.write(f"🎡 [Download wheel file]({s3_url})")
+
 st.header("Edit my source 👇")
 execbox(
     """
