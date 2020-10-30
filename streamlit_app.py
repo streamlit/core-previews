c6e4b519e94ac5e55f9ee8dfc93106a5fc96cd08
karrie <karrie@streamlit.io>
2020-10-30 02:04:01 -0400

    Add selector for E2E tests to prepopulate execbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,37 @@
 import streamlit as st
 from execbox import execbox
+from e2e_loader import select_script, get_script
 
 with open('requirements.txt') as requirements:
-    s3_url = requirements.read().split('\n')[2]
+    s3_url = requirements.read().split('\n')[-2]
 import re
-match = re.search('/pr-(\\d+)/', s3_url)
-if match: 
-    pr_number = match.group(1)
-    
-    col1, col2 = st.beta_columns(2)
-    col1.write(f"🔭 [View PR on Github](https://github.com/streamlit/streamlit/pull/{pr_number})")
-    col2.write(f"🎡 [Download wheel file]({s3_url})")
 
-st.header("Edit my source 👇")
-execbox(
-    """
+col1, padding, col2 = st.beta_columns([3, .1, 1])
+
+match = re.search('/pr-(\\d+)/', s3_url)
+if match:
+    pr_number = match.group(1)
+
+    col2.write(f"🔭 [View PR on Github](https://github.com/streamlit/streamlit/pull/{pr_number})")
+    col2.write(f"🎡 [Download wheel]({s3_url})")
+
+with col1:
+    selected_script = select_script()
+
+if selected_script:
+    script = get_script(selected_script["download_url"])
+    # st.write("placeholder", script)
+else:
+    script = """
 import streamlit as st
 
 st.header("Results")
 if st.button("Press me!"):
     st.balloons()
-""",
+    """
+
+st.header("Edit my source 👇")
+execbox(script,
     autorun=True,
     line_numbers=True,
     height=300,
