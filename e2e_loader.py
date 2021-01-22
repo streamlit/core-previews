2a7de840d7467835730f4470b30de8936fb8456f
bh-streamlit <76131205+bh-streamlit@users.noreply.github.com>
2021-01-21 16:07:50 -0800

    Fix get_scripts not pulling up new e2e scripts (#4)
    * squash into one commit
    
    * fix identifier

--- e2e_loader.py
+++ e2e_loader.py
@@ -1,17 +1,21 @@
 import requests
 import streamlit as st
 
+
 @st.cache(suppress_st_warning=True, allow_output_mutation=True)
-def get_scripts(fork="streamlit", branch="develop", headers={}):
+def get_scripts(fork=None, branch=None, headers={}):
+    if fork is None:
+        fork = "streamlit"
+    if branch is None:
+        branch = "develop"
+
     response = requests.get(
         f"https://api.github.com/repos/{fork}/streamlit/contents/e2e/scripts?ref={branch}",
-        headers=headers
+        headers=headers,
     )
-    response_body = response.json()
     if response.status_code == 200:
-        return response_body
-    else:
-        raise Error
+        return response.json()
+
 
 @st.cache(suppress_st_warning=True)
 def get_script(url):
@@ -19,30 +23,55 @@ def get_script(url):
     e2e_script = response.text
     return e2e_script
 
+
 def set_auth():
     gh_token = st.text_input("Enter your GH token", key="key")
     if gh_token:
-        return {'Authorization': 'token %s' % gh_token}
+        return {"Authorization": "token %s" % gh_token}
 
 
-def select_script(auth={}):
+@st.cache(suppress_st_warning=True)
+def get_pr_info(pr_number):
+    r = requests.get(
+        f"https://api.github.com/repos/streamlit/streamlit/pulls/{pr_number}"
+    )
     try:
-        scripts = get_scripts(headers=auth)
+        body = r.json()
+        fork = body["head"]["user"]["login"]
+        branch = body["head"]["ref"]
+        return fork, branch
     except:
+        return None, None
+
+
+def select_script(branch, pr_number, auth={}):
+    # From the S3 url, we can be passed either a branch ("<branch>-preview")
+    # or a PR ("pr-<pr>"). (See get_branch_info() in streamlit_app.py.)
+    #
+    # If we have a branch, use streamlit's fork (default).
+    # If we have a PR, use Github's API to get the fork and branch.
+    if pr_number is not None:
+        fork, branch = get_pr_info(pr_number)
+    else:
+        fork = None
+
+    scripts = None
+    try:
+        scripts = get_scripts(fork=fork, branch=branch, headers=auth)
+    except Exception as e:
         auth_headers = set_auth()
         if auth_headers:
-            scripts = get_scripts(headers=auth_headers)
+            scripts = get_scripts(fork=fork, branch=branch, headers=auth_headers)
 
     if scripts:
         return render_script_selector(scripts)
 
+
 def render_script_selector(scripts):
-    placeholder_option = [{ "name": "Please select a script"}]
+    placeholder_option = [{"name": "Please select a script"}]
     options = placeholder_option + scripts
     selected_script = st.selectbox(
-        "Select E2E script",
-        options= options,
-        format_func=lambda x: x["name"]
+        "Select E2E script", options=options, format_func=lambda x: x["name"]
     )
     if selected_script and "download_url" in selected_script:
         return selected_script
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,40 @@
 import streamlit as st
 from execbox import execbox
 from e2e_loader import select_script, get_script
+from urllib.parse import urlparse
 
-with open('requirements.txt') as requirements:
-    s3_url = requirements.read().split('\n')[-2]
+with open("requirements.txt") as requirements:
+    s3_url = requirements.read().split("\n")[-2]
 import re
 
-col1, padding, col2 = st.beta_columns([3, .1, 1])
+col1, padding, col2 = st.beta_columns([3, 0.1, 1])
 
-match = re.search('/pr-(\\d+)/', s3_url)
-if match:
-    pr_number = match.group(1)
 
-    col2.write(f"🔭 [View PR on Github](https://github.com/streamlit/streamlit/pull/{pr_number})")
+def get_first_match(regex, s):
+    match = re.search(regex, s)
+    return match.group(1) if match else None
+
+
+def get_branch_info():
+    path = urlparse(s3_url).path
+    path_parts = list(filter(None, path.split("/")))
+    core_preview_branch = "/".join(path_parts[:-1])
+
+    branch = get_first_match("(.*)-preview", core_preview_branch)
+    pr = get_first_match("pr-(\\d+)", core_preview_branch)
+
+    return branch, pr
+
+
+branch, pr_number = get_branch_info()
+if pr_number is not None:
+    col2.write(
+        f"🔭 [View PR on Github](https://github.com/streamlit/streamlit/pull/{pr_number})"
+    )
     col2.write(f"🎡 [Download wheel]({s3_url})")
 
 with col1:
-    selected_script = select_script()
+    selected_script = select_script(branch, pr_number)
 
 if selected_script:
     script = get_script(selected_script["download_url"])
@@ -30,7 +48,8 @@ if st.button("Press me!"):
     """
 
 st.header("Edit my source 👇")
-execbox(script,
+execbox(
+    script,
     autorun=True,
     line_numbers=True,
     height=300,
