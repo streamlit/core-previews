6380668f285f21e240c3e5b0567c311e171b60c0
Brandon Hsiao <bh@streamlit.io>
2021-02-04 15:16:51 -0800

    add example

--- default_script.py
+++ default_script.py
@@ -1,5 +1,8 @@
+import pandas as pd
+import numpy as np
 import streamlit as st
 
-st.header("Results")
-if st.button("Press me!"):
-    st.balloons()
+df = pd.DataFrame({f"col_{i+1}": np.random.random(size=5) for i in range(30)})
+expander = st.beta_expander("Look at the bug", True)
+expander.table(df)
+
