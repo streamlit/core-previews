46d8d812f2ee253d6f4d17340c3f426ffa9eeb40
Brandon Hsiao <bh@streamlit.io>
2021-02-04 14:49:28 -0800

    update default script

--- default_script.py
+++ default_script.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
-st.header("Results")
-if st.button("Press me!"):
-    st.balloons()
+image_url = "https://i.guim.co.uk/img/media/86c3481516dce247943ac2978b4f48d16a3ac265/0_170_5120_3074/master/5120.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=637dc5731d52754675ef36344a6af3c8"
+
+
+col1, col2 = st.beta_columns(2)
+
+with col1:
+    for _ in range(3):
+        st.image(image_url)
+
+with col2:
+    st.image(image_url)
+    st.markdown("# Some header 1")
+    st.markdown("## Some header 2")
+    st.markdown("### Some header 3")
