3689a10801cb0104ce97b244200ca6f59dcf87d1
Brandon Hsiao <bh@streamlit.io>
2021-02-08 15:42:21 -0800

    update default_script.py

--- default_script.py
+++ default_script.py
@@ -1,5 +1,51 @@
 import streamlit as st
 
-st.header("Results")
-if st.button("Press me!"):
-    st.balloons()
+sp1, row1, sp2, row2, sp3 = st.beta_columns((.1, 2, .2, 1, .1))
+
+with row1:
+    st.markdown('''
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec laoreet dolor a
+quam fringilla, blandit mollis est dignissim. Proin eget eros maximus, gravida
+justo eu, vehicula mi. Pellentesque nec auctor lectus, sit amet porttitor
+risus. Curabitur nec sagittis tortor, feugiat sodales tellus. Suspendisse ac
+tincidunt metus, congue auctor justo. Fusce ac mauris mauris. Vestibulum ac
+cursus erat, ac facilisis nulla. Nam quis dui efficitur, ultricies ante non,
+finibus nisi. Integer accumsan sapien vitae ligula luctus pulvinar. Etiam sed
+volutpat urna. Morbi blandit pretium porttitor. Quisque nec fringilla felis.
+Nunc eros nisi, faucibus in blandit sit amet, euismod in turpis. Maecenas
+blandit leo sapien. Aenean id consectetur ex.
+
+Maecenas mollis mi vel ligula dapibus pharetra. Maecenas feugiat mi nibh, in
+posuere orci suscipit et. Fusce eu eros tincidunt, varius leo sed, aliquet
+erat. Aliquam imperdiet ullamcorper diam in convallis. Donec finibus nisl ac ex
+suscipit gravida. Sed ut pretium ipsum, eget imperdiet lacus. Pellentesque
+rhoncus, eros vel tristique lobortis, risus neque finibus leo, eget sodales
+ante erat at ligula. Aenean eu mauris dictum, ultricies eros sed, scelerisque
+sem. Quisque molestie at neque quis varius. Curabitur at semper nibh. Nulla
+ultricies porttitor massa eu pellentesque. Nullam eget nulla sodales, blandit
+quam nec, pulvinar tellus. Integer sed ligula elit. ''')
+
+
+with row2:
+    st.markdown('''
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec laoreet dolor a
+quam fringilla, blandit mollis est dignissim. Proin eget eros maximus, gravida
+justo eu, vehicula mi. Pellentesque nec auctor lectus, sit amet porttitor
+risus. Curabitur nec sagittis tortor, feugiat sodales tellus. Suspendisse ac
+tincidunt metus, congue auctor justo. Fusce ac mauris mauris. Vestibulum ac
+cursus erat, ac facilisis nulla. Nam quis dui efficitur, ultricies ante non,
+finibus nisi. Integer accumsan sapien vitae ligula luctus pulvinar. Etiam sed
+volutpat urna. Morbi blandit pretium porttitor. Quisque nec fringilla felis.
+Nunc eros nisi, faucibus in blandit sit amet, euismod in turpis. Maecenas
+blandit leo sapien. Aenean id consectetur ex.
+
+Maecenas mollis mi vel ligula dapibus pharetra. Maecenas feugiat mi nibh, in
+posuere orci suscipit et. Fusce eu eros tincidunt, varius leo sed, aliquet
+erat. Aliquam imperdiet ullamcorper diam in convallis. Donec finibus nisl ac ex
+suscipit gravida. Sed ut pretium ipsum, eget imperdiet lacus. Pellentesque
+rhoncus, eros vel tristique lobortis, risus neque finibus leo, eget sodales
+ante erat at ligula. Aenean eu mauris dictum, ultricies eros sed, scelerisque
+sem. Quisque molestie at neque quis varius. Curabitur at semper nibh. Nulla
+ultricies porttitor massa eu pellentesque. Nullam eget nulla sodales, blandit
+quam nec, pulvinar tellus. Integer sed ligula elit. ''')
+
