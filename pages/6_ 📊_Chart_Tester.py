da89ee4962ea05e5d1bf0a57358abf4cb49deec9
lukasmasuch <lukas.masuch@gmail.com>
2025-03-14 12:44:03 +0100

    Update

--- pages/6_ 📊_Chart_Tester.py
+++ pages/6_ 📊_Chart_Tester.py
@@ -60,14 +60,7 @@ fig = ff.create_distplot(hist_data, group_labels, bin_size=[0.1, 0.25, 0.5])
 st.write("**Plotly Chart:**")
 st.plotly_chart(fig, use_container_width=True)
 
-
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
-
-p = figure(title="simple line example", x_axis_label="x", y_axis_label="y")
-
-p.line(x, y, legend_label="Trend", line_width=2)
-
+# Pydeck
 df = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=["lat", "lon"]
 )
